bafddb8082cdec9436f4ce32b5f1ed6df0c860d4
윤성 <yskimno1@gmail.com>
2019-05-03 20:08:02 +0900

    base of frame table insertion done

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "vm/frame.h"
+#include "threads/thread.h"
+#include "threads/palloc.h"
 
 /*
  * Initialize frame table
@@ -8,17 +10,38 @@
 void 
 frame_init (void)
 {
-
+    list_init(&frame_table);
+    /* Needs frame lock */
+    lock_init(&lock_frame);
 }
 
+void
+insert_frame_table(struct frame_table_entry* fte){
+    list_push_back(&frame_table, fte->elem_table_list);
+}
+
+struct frame_table_entry*
+create_frame_table_entry(void* frame){
+    struct frame_table_entry* fte = malloc(sizeof(struct frame_table_entry));
+    fte->frame = frame;
+    fte->owner = thread_current();
+    /* spte should be initialized! */
+
+    return fte;
+}
 
 /* 
  * Make a new frame table entry for addr.
  */
 bool
-allocate_frame (void *addr)
+allocate_frame (void *addr, enum palloc_flags flag)
 {
-
-    return false;
+    ASSERT(flag & PAL_USER);
+    void* frame = palloc_get_page(flag);
+    struct frame_table_entry* fte = create_frame_table_entry(frame);
+    lock_acquire(&lock_frame);
+    insert_frame_table(fte);
+    lock_release(&lock_frame);
+    return true;
 }
 
--- pintos/src/vm/frame.h
+++ pintos/src/vm/frame.h
@@ -1,17 +1,23 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include "threads/palloc.h"
 
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
+
+struct list frame_table;
+struct lock lock_frame;
 
 struct frame_table_entry
 {
 	uint32_t* frame;
 	struct thread* owner;
 	struct sup_page_table_entry* spte;
+
+	struct list_elem* elem_table_list;
 };
 
 void frame_init (void);
-bool allocate_frame (void *addr);
+bool allocate_frame (void *addr, enum palloc_flags flag);
 
 #endif /* vm/frame.h */
