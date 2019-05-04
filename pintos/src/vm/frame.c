383b2a57258f71dd30f89fa4a49aeefb1c5504ef
윤성 <yskimno1@gmail.com>
2019-05-05 02:25:57 +0900

    test...

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -45,6 +45,8 @@ allocate_frame (struct sup_page_table_entry* spt_e, enum palloc_flags flag)
 {
     ASSERT(flag & PAL_USER);
     uint8_t* frame = palloc_get_page(flag);
+    printf("frame : %d", frame);
+    printf("%d", frame==NULL);
     // if(frame == NULL) PANIC("frame is full");
     struct frame_table_entry* fte = create_frame_table_entry(frame, spt_e);
     if(fte == NULL) return NULL;
