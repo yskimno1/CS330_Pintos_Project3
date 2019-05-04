752962cb3fd3c5af8807985f9b142c16b799d02e
윤성 <yskimno1@gmail.com>
2019-05-05 00:35:34 +0900

    handling a tiny page fault condition

--- pintos/src/userprog/exception.c
+++ pintos/src/userprog/exception.c
@@ -150,18 +150,29 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  printf("page fault!\n");
+  void* success;
+  if(is_user_vaddr(fault_addr) && not_present){
+     success = grow_stack(fault_addr);
+  }
+  if(success == NULL){
+   printf ("Page fault at %p: %s error %s page in %s context.\n",
+            fault_addr,
+            not_present ? "not present" : "rights violation",
+            write ? "writing" : "reading",
+            user ? "user" : "kernel");
+   kill (f);
+  }
   exit(-1);
-  printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
+//   printf ("Page fault at %p: %s error %s page in %s context.\n",
+//           fault_addr,
+//           not_present ? "not present" : "rights violation",
+//           write ? "writing" : "reading",
+//           user ? "user" : "kernel");
 
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
 
-  kill (f);
+//   kill (f);
 }
 
