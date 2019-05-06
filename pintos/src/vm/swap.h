17379a426f0ad443605511bb39ad2661fddc3d2a
윤성 <yskimno1@gmail.com>
2019-05-06 18:52:29 +0900

    modified swap.h

--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -1,12 +1,13 @@
-#ifndef VM_SWAP_H
-#define VM_SWAP_H
-
+#include "vm/page.h"
+#include "vm/frame.h"
+#include "devices/disk.h"
 
 void swap_init (void);
 bool swap_in (void *addr);
 bool swap_out (void);
 void read_from_disk (uint8_t *frame, int index);
 void write_to_disk (uint8_t *frame, int index);
-
+#ifndef VM_SWAP_H
+#define VM_SWAP_H
 
 #endif /* vm/swap.h */
