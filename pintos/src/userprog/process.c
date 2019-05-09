df1ce7311ce1f78871ba00b4e71bbfdbc3a66984
윤성 <yskimno1@gmail.com>
2019-05-09 18:47:48 +0900

    test with palloc_get_page

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -607,6 +607,7 @@ install_page (void *upage, void *kpage, bool writable)
 
   /* Verify that there's not already a page at that virtual
      address, then map our page there. */
+  printf("pagedir : %p", t->pagedir);
   return (pagedir_get_page (t->pagedir, upage) == NULL
           && pagedir_set_page (t->pagedir, upage, kpage, writable));
 }
