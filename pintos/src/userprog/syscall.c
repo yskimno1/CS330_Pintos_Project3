bc3f3b0ffe20d0fda153bddfd2ae83dd90db62bf
윤성 <yskimno1@gmail.com>
2019-05-10 12:39:56 +0900

    changed munmap

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -535,9 +535,9 @@ void munmap(int mapid){
 	// printf("unmap came!\n");
 	lock_acquire(&lock_frame);
 	struct list_elem* e;
-	if(!list_empty(&thread_current()->list_mmap)){
-		for(e=list_begin(&thread_current()->list_mmap); e!=list_end(&thread_current()->list_mmap); e=list_next(e)){
-			struct page_mmap* mmap_e = list_entry(e,struct page_mmap, elem_mmap);
+	e = list_begin(&thread_current()->list_mmap);
+	while(e!=list_end(&thread_current()->list_mmap)){
+		struct page_mmap* mmap_e = list_entry(e,struct page_mmap, elem_mmap);
 			// printf("map_id : %d, mapid : %d\n", mmap_e->spt_e->map_id, mapid);
 			if(mmap_e->spt_e->map_id == mapid){
 				// printf("begin! map id : %d, addr %p\n", mapid, mmap_e->spt_e);
@@ -545,15 +545,17 @@ void munmap(int mapid){
 					file_write_at(mmap_e->spt_e->file, mmap_e->spt_e->user_vaddr, mmap_e->spt_e->read_bytes, mmap_e->spt_e->offset);
 					free_frame(pagedir_get_page(thread_current()->pagedir, mmap_e->spt_e->user_vaddr));
 					free_page(&mmap_e->spt_e->elem);
-					list_remove(e);
+					e = list_remove(e);
 				}
 				else{
 					free_frame(pagedir_get_page(thread_current()->pagedir, mmap_e->spt_e->user_vaddr));
 					free_page(&mmap_e->spt_e->elem);
-					list_remove(e);
+					e = list_remove(e);
 				}
 			}
-		}
+			else{
+				e = list_next(e);
+			}
 	}
 
 	lock_release(&lock_frame);
