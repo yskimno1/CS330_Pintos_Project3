81b03603b9b903cfdfc1c5d7d9bc3066cd6ebdd2
윤성 <yskimno1@gmail.com>
2019-05-11 22:37:04 +0900

    please

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -162,18 +162,17 @@ process_exit (void)
   curr->is_exited = true;
 
   /* unmap all */
-  filelock_acquire();
+
   if(!list_empty(&thread_current()->list_mmap)){
 		for(e=list_begin(&thread_current()->list_mmap); e!=list_end(&thread_current()->list_mmap); e=list_next(e)){
 			struct page_mmap* mmap_e = list_entry(e,struct page_mmap, elem_mmap);
       if(pagedir_is_dirty(thread_current()->pagedir, mmap_e->spt_e->user_vaddr)){
+        filelock_acquire();
         file_write_at(mmap_e->spt_e->file, mmap_e->spt_e->user_vaddr, mmap_e->spt_e->read_bytes, mmap_e->spt_e->offset);
+        filelock_release();
       }
     }
 	}
-  filelock_release();
-
-  
 
   sema_up(&curr->sema_wait);
   /* wait until parent removes the child in the list */
